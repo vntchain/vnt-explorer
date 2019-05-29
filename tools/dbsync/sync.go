e89748e59ac74ff1314aafb2eab56149e9abad6e
Feng Xi Yan <goodyfx@163.com>
2019-05-29 12:04:12 +0800

    Add prompt for dbsync

--- tools/dbsync/sync.go
+++ tools/dbsync/sync.go
@@ -5,13 +5,54 @@ import (
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	"github.com/vntchain/vnt-explorer/models"
+	"strings"
+	"github.com/gookit/color"
 )
 
 func main() {
 	orm.RegisterDriver("mysql", orm.DRMySQL)
 
-	orm.RunSyncdb("default", true, true)
-	alterTable()
+	dbuser := beego.AppConfig.String("mysql::user")
+	dbhost := beego.AppConfig.String("mysql::host")
+	dbport := beego.AppConfig.String("mysql::port")
+	dbname := beego.AppConfig.String("mysql::db")
+
+	red := color.FgRed.Render
+	green := color.FgGreen.Render
+
+	fmt.Println(green("本操作将会对以下数据库进行数据库重建操作："))
+	fmt.Println(green("服务器："), dbhost)
+	fmt.Println(green("端口："), dbport)
+	fmt.Println(green("用户名："), dbuser)
+	fmt.Println(green("数据库："), dbname)
+
+	fmt.Println(red("危险！危险！危险！本操作会将当前数据库清空！"))
+	fmt.Println(red("如果是线上数据库，建议您采用migration的方式进行数据库升级！\n"))
+
+	var alert = []string{
+		"你真的要这么做吗？", "一定要想清楚啊！",
+		"不要冲动啊！", "你确定知道你在做什么吗？",
+		"没有后悔药哦！", "好吧，看起来你很坚定！",
+		"嗯，看好你哦！", "好吧，我们开始一个全新的世界吧！",
+		"要不要再想一下？", "好的，let's go!",
+	}
+
+	var command string
+	for _, console := range(alert) {
+		fmt.Print(red(console), green("Y(继续)/N(放弃):"))
+		fmt.Scan(&command)
+		if strings.ToUpper(command) == "Y" {
+			continue
+		} else {
+			fmt.Println("放弃！")
+			break
+		}
+	}
+
+	if strings.ToUpper(command) == "Y" {
+		orm.RunSyncdb("default", true, true)
+		alterTable()
+	}
 }
 
 func registerModel() {
